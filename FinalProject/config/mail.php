4cd8a7abdd4b3b51505ca3b54f3dd23f0636a283
2025whystillnoLoginwGoogle <krispyyesg.06@gmail.com>
2026-05-18 01:17:54 +0800

    Add app password to mail configuration
    Updated the email configuration to include a password.

--- FinalProject/config/mail.php
+++ FinalProject/config/mail.php
@@ -3,7 +3,7 @@
 return[
     'host' => 'smtp.gmail.com',
     'username' => 'jaredesguerra.backup@gmail.com',
-    'password' => '',
+    'password' => 'fqdn sxca lubt hfoy',
     'port' => 587,
     'encryption' => 'tls',
     'from_email' => 'jaredesguerra.backup@gmail.com',
